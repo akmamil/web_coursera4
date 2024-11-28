02bc66596cb7831c1f1ce12a0ee89447d4b312d2
Akmaral <akmakuroo47@gmail.com>
2024-11-28 16:57:27 +0500

    Final

---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+(function () {
+  var speakWord = "Hello";
+
+  var helloSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.helloSpeaker = helloSpeaker;
+})();
